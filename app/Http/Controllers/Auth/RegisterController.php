0ae05f1222ed7af0d0657a313828fc65da223968
maximtrunnikov <m.trunnikov@mail.ru>
2021-08-27 02:32:15 +0300

    Stop verification x3

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -74,6 +74,7 @@ class RegisterController extends Controller
 
         Auth::login($user);
 
-        return Redirect::route('verification.notice');
+        return Redirect::route('dashboard');
+//        return Redirect::route('verification.notice');
     }
 }
