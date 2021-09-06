d6ad5c33b775f4d9a304553c6a1af0c4167bbcc5
maximtrunnikov <m.trunnikov@mail.ru>
2021-09-06 18:48:35 +0300

    Fix migrations and models x2

--- app/Models/Users/Country.php
+++ app/Models/Users/Country.php
@@ -2,23 +2,18 @@
 
 namespace App\Models\Users;
 
+use App\Traits\HasTranslations;
 use App\Traits\HasUsersTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use App\Traits\HasTranslations;
 
-class City extends Model
+class Country extends Model
 {
     use HasTranslations;
 
     public $translatable = ['name'];
-    protected $fillable = ['name', 'area_id'];
+    protected $fillable = ['name', 'code', 'currency_id'];
     public $timestamps = false;
     use HasFactory, HasUsersTrait;
-
-    public function area(): BelongsTo
-    {
-        return $this->belongsTo(Area::class);
-    }
 }
