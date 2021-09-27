c82223aaaf65fe686ed1bae567bf9796b0a0ee3b
maximtrunnikov <m.trunnikov@mail.ru>
2021-09-27 20:59:07 +0300

    Selections dashboard test

--- resources/js/src/Pages/Selections/Dashboard.js
+++ resources/js/src/Pages/Selections/Dashboard.js
@@ -38,45 +38,35 @@ const Dashboard = () => {
     }
     const cards = [
         {
-            title: Lang.get('pages.selections.dashboard.single_prefs'),
-            src: imgPath + paths.singlePump + producers.singe + ext,
+            title: Lang.get('pages.selections.dashboard.preferences.pump.single'),
+            src: imgPath + "01.jpg",
             onClick: () => {
                 Inertia.get(tRoute('selections.create', project_id))
             }
         },
         {
-            title: Lang.get('pages.selections.dashboard.double_prefs'),
-            src: imgPath + paths.doublePump + producers.double + ext,
+            title: Lang.get('pages.selections.dashboard.preferences.pump.double'),
+            src: imgPath + "02.jpg",
             onClick: serviceUnavailable
         },
         {
-            title: Lang.get('pages.selections.dashboard.water_station_prefs'),
-            src: imgPath + paths.station.water + producers.station.water + ext,
+            title: Lang.get('pages.selections.dashboard.preferences.pump.borehole'),
+            src: imgPath + "03.jpg",
             onClick: serviceUnavailable
         },
         {
-            title: Lang.get('pages.selections.dashboard.fire_station_prefs'),
-            src: imgPath + paths.station.fire + producers.station.fire + ext,
+            title: Lang.get('pages.selections.dashboard.preferences.pump.drainage'),
+            src: imgPath + "04.jpg",
             onClick: serviceUnavailable
         },
         {
-            title: Lang.get('pages.selections.dashboard.single_analogy'),
-            src: imgPath + paths.singlePump + producers.singe + ext,
+            title: Lang.get('pages.selections.dashboard.preferences.station.water'),
+            src: imgPath + "05.jpg",
             onClick: serviceUnavailable
         },
         {
-            title: Lang.get('pages.selections.dashboard.double_analogy'),
-            src: imgPath + paths.doublePump + producers.double + ext,
-            onClick: serviceUnavailable
-        },
-        {
-            title: Lang.get('pages.selections.dashboard.water_station_analogy'),
-            src: imgPath + paths.station.water + producers.station.water + ext,
-            onClick: serviceUnavailable
-        },
-        {
-            title: Lang.get('pages.selections.dashboard.fire_station_analogy'),
-            src: imgPath + paths.station.fire + producers.station.fire + ext,
+            title: Lang.get('pages.selections.dashboard.preferences.station.fire'),
+            src: imgPath + "06.jpg",
             onClick: serviceUnavailable
         },
     ]
@@ -91,9 +81,9 @@ const Dashboard = () => {
                 ? tRoute('projects.show', project_id)
                 : tRoute('projects.index')}
         >
-            <Row justify="space-around" gutter={[0, 10]}>
+            <Row justify="space-around" gutter={[48, 16]}>
                 {cards.map(card => (
-                    <Col key={card.title} xs={11} md={5}>
+                    <Col key={card.title} lg={11} xl={8}>
                         <DashboardCard {...card} key={card.title}/>
                     </Col>
                 ))}
