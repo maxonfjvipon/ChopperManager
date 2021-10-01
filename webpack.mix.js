b083e325d6937a51d1cc4bac5cc87aec1ce78869
maximtrunnikov <m.trunnikov@mail.ru>
2021-10-01 17:18:48 +0300

    version?

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     // .css('resources/css/app.css', 'public/css')
     .react()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .version()
+;
