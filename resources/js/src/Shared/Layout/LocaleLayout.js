db5cbe006961196f2d5b7bf166c8fc58c281bb42
maximtrunnikov <m.trunnikov@mail.ru>
2021-10-04 00:01:08 +0300

    Ant components localization

--- resources/js/src/Shared/Layout/LocaleLayout.js
+++ resources/js/src/Shared/Layout/LocaleLayout.js
@@ -1,9 +1,19 @@
 import {usePage} from "@inertiajs/inertia-react";
 import Lang from "../../../translation/lang";
+import {ConfigProvider} from "antd";
+import ruRU from 'antd/lib/locale/ru_RU'
+import enUS from 'antd/lib/locale/en_US'
 
 export const LocaleLayout = ({children}) => {
     const {locales} = usePage().props
     Lang.setLocale(locales.current)
 
-    return children
+    const _locales = {
+        'ru': ruRU,
+        'en': enUS,
+    }
+
+    return <ConfigProvider locale={_locales[locales.current]}>
+        {children}
+    </ConfigProvider>
 }
