398a8b54710e96199e6e24adb7731bb8281e0d8b
maximtrunnikov <m.trunnikov@mail.ru>
2022-02-04 18:15:49 +0300

    #9 try CI x2

--- Modules/Core/Endpoints/ProjectsCloneEndpoint.php
+++ Modules/Core/Endpoints/ProjectsCloneEndpoint.php
@@ -4,13 +4,8 @@ namespace Modules\Core\Endpoints;
 
 use App\Takes\TkAuthorized;
 use App\Http\Controllers\Controller;
-use App\Support\Take;
-use App\Takes\TkWithCallback;
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Contracts\Support\Responsable;
-use Illuminate\Http\Request;
 use Modules\Core\Takes\TkAuthorizedProject;
-use Modules\Core\Takes\TkClonedProject;
 use Modules\Core\Takes\TkRedirectedToProjectsIndex;
 use Modules\Core\Entities\Project;
 use Modules\Core\Http\Requests\CloneProjectRequest;
